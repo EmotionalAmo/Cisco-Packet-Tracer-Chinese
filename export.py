8bdc22ca854ca82b60f88e438c46448cafa35a58
EmotionalAmo <emotionalamo@outlook.com>
2024-10-31 13:13:41 +0400

    更新最新版本 8.2.2 的汉化文件

--- export.py
+++ export.py
@@ -1,6 +1,7 @@
 from xml.etree import ElementTree as ET
 from pygtrans import Translate
 
+
 global temp
 
 
@@ -12,8 +13,25 @@ tree = ET.parse("template.ts")
 
 for children in tree.iter():
     if children.tag == 'source':
-        temp = trans(str(children.text))
+        if str(children.text).lower() == 'auto':
+            temp = '自动'
+        elif str(children.text).lower() == 'strings':
+            temp = '字符串'
+        elif str(children.text).lower() == 'min':
+            temp = '最小'
+        elif str(children.text).lower() == 'max':
+            temp = '最大'
+        elif str(children.text).lower() == 'mask':
+            temp = '掩码'
+        elif str(children.text).lower() == 'value':
+            temp = '值'
+        elif str(children.text).lower() == 'status':
+            temp = '状态'
+        else:
+            temp = trans(str(children.text))
+        print(str(children.text))
     if children.tag == 'translation':
         children.text = str(temp)  # type: ignore
+        print(temp)
 
-tree.write('chinese.1.8.1.ts', encoding='UTF-8')
+tree.write('chinese.8.2.2.0400.ts', encoding='UTF-8')
